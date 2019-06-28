18ee16c90d0e2e832ba8d41771a9a824a08f89db
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-06-28 11:27:57 +0200

    loading ontologie

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,28 @@
-print("Hi, WatchDogs!")
+import rdflib 
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+from rdflib.namespace import DC, FOAF
+
+
+
+g = Graph()
+
+# Create an identifier to use as the subject for Donna.
+donna = BNode()
+
+# Add triples using store's add method.
+g.add( (donna, RDF.type, FOAF.Person) )
+g.add( (donna, FOAF.nick, Literal("donna", lang="foo")) )
+g.add( (donna, FOAF.name, Literal("Donna Fales")) )
+g.add( (donna, FOAF.mbox, URIRef("mailto:donna@example.org")) )
+
+# Iterate over triples in store and print them out.
+print("--- printing raw triples ---")
+
+for person in g.subjects(RDF.type, FOAF.Person):
+    for mbox in g.objects(person, FOAF.mbox):
+        print(mbox)
+#for s, p, o in g:
+    #print((s, p, o))
+
+
+g.serialize(destination='xmlout.xml', format='xml')
