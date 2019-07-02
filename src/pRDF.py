239589bbcd250d4eec98212dea38ff7aa0dec610
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-07-02 18:38:42 +0200

    open fix on number of entity

--- src/pRDF.py
+++ src/pRDF.py
@@ -11,73 +11,96 @@ from rdflib.namespace import XSD
 
 import re 
 
+import xml.etree.ElementTree as ET
 
 
-# add predicato-obj transitivo  
-def parseToGraph(g,_from,to): 
+entity = {}
+'''
+def graph_toRdf():
+    root = ET.Element("rdf:RDF")
+    doc = ET.SubElement(root, "rdf:Description")
+    
+    ET.SubElement(doc, "field1", name="blah").text = "some value1"
+    ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+    
+    tree = ET.ElementTree(root)
+    tree.write("serCustom.xml")
+'''
+
+#single target 
+def entity_map(p,o,target):
+    if(target in o):
+        s = o.split('/')
+        u = "" 
+        for i in range(0,len(s)-1):
+            u += s[i] + "/"
+        #print(u)
+        entity[p] = u
+
+
+def filter(s,p,o):
+    s = s.strip().replace("//","/").split('/')
+    s = s[len(s)-1]
+    p = p.strip().replace("//","/")
+    p = re.split('~|#|/',p)
+    p = p[len(p)-1]
+    o = o.strip().replace("//","/")
+    o = re.split('~|#|/',o)
+    o = o[len(o)-1]
+    return s,p,o
+
+
+     
+
+
+
+def parseToGraph(g,_from,to,target=None): 
     dsub = {}
     to_list = {}
 
     network = nx.MultiDiGraph()  
 
-
     for s,p,o in g: #da pdm lo so 
-        s = s.strip().replace("//","/").split('/')
-        sub = s[len(s)-1]
-        p = p.strip().replace("//","/")
-        p = re.split('~|#|/',p)
-        pre = p[len(p)-1]
-        o = o.strip().replace("//","/")
-        o = re.split('~|#|/',o)
-        o = o[len(o)-1]
-        if(pre in to): #predicati 
-            if(o not in network.nodes()):
-                network.add_node(o) # generi univoci nel grafo 
-            to_list[sub] = o  #i soggetti con i relativi generi 
+        s,pr,_o = filter(s,p,o)
+        #entity_map(pr,o,"imdb")
+        if(pr in to): #predicati 
+            if(_o not in network.nodes()):
+                network.add_node(_o) # generi univoci nel grafo 
+            to_list[s] = _o  #i soggetti con i relativi generi 
 
     for s,p,o in g:
-        s = s.strip().replace("//","/").split('/')
-        sub = s[len(s)-1]
-        
-        p = p.strip().replace("//","/")
-        p = re.split('~|#|/',p)
-        pre = p[len(p)-1]
-        o = o.strip().replace("//","/")
-        o = re.split('~|#|/',o)
-        o = o[len(o)-1]
-        
-        print(sub,pre,o)
+        s,p,o = filter(s,p,o)
 
-        if(sub  in dsub.keys()):
-            if(pre in dsub[sub].keys()): 
+        if(s in dsub.keys()):
+            if(p in dsub[s].keys()): 
                 #predicato gia inserito 
-                if(type(dsub[sub][pre])  is list):
-                    dsub[sub][pre].append(o)
+                if(type(dsub[s][p])  is list):
+                    dsub[s][p].append(o)
                 else:
-                    tmp = dsub[sub][pre]
-                    del dsub[sub][pre]
-                    dsub[sub][pre] = []
-                    dsub[sub][pre].append(tmp)
-                    dsub[sub][pre].append(o)
+                    tmp = dsub[s][p]
+                    del dsub[s][p]
+                    dsub[s][p] = []
+                    dsub[s][p].append(tmp)
+                    dsub[s][p].append(o)
             else:
                 #predicato sconosciuto 
-                dsub[sub][pre] = o
+                dsub[s][p] = o
         else:
-            dsub[sub] = {}
-            dsub[sub][pre] = o
+            dsub[s] = {}
+            dsub[s][p] = o
         
         #graph 
-        if(pre in _from):
+        if(p in _from):
             if(o not in network.nodes()):
                 network.add_node(o)
-            to_item = to_list[sub]
+            to_item = to_list[s]
             if(network.has_edge(o,to_item)):
                 w = network.get_edge_data(o,to_item)
                 w = w[0]["weight"] 
                 network[o][to_item][0]["weight"] = w + 1
                 #print(network.get_edge_data(o,to_item))
             else:
-                network.add_edge(o,to_item,attr=pre,weight=1)    
+                network.add_edge(o,to_item,attr=p,weight=1)    
         
     '''
     network.add_edge("Pixar","hoodie",weight=21)
@@ -93,9 +116,15 @@ def parseToGraph(g,_from,to):
         except:
             print(u,v)
     print("\n\n\n\n\n\n\n\n")
-    print(network["nm0002071"]) #nm0002071
+    #print(network["nm0002071"]) #nm0002071
+    #print(network["hoodie"]) 
+
+    #print(len(network.nodes()))
     return network
     
+
+
+
 
 film = rdflib.URIRef('http://www.example.org/tt001')
 
@@ -104,46 +133,44 @@ film1 = rdflib.URIRef('http://www.example.org/tt002')
 film2 = rdflib.URIRef('http://www.example.org/tt003')
 
 
-pr = rdflib.URIRef('http://www.example.org/~attr_prob')
-id = rdflib.URIRef('http://www.example.org/unique_id')
-
+#entita 
 attore = rdflib.URIRef('http://www.example.org/attore')
 genere = rdflib.URIRef('http://www.example.org/genere')
 direttore = rdflib.URIRef('http://www.example.org/direttore')
 autore = rdflib.URIRef('http://www.example.org/autore')
 
 g = ConjunctiveGraph()
-g.parse("/home/phinkie/Scrivania/turbo-watchdogs/ontologie/film.xml", format="xml")
-G = parseToGraph(g,["actor","director","author"],["genre"])
-'''
+#g.parse("/home/phinkie/Scrivania/turbo-watchdogs/ontologie/film.xml", format="xml")
+#G = parseToGraph(g,["actor","director","author"],["genre"])
+#decoder(g)
+
+
 #info generali 
 g.add((film,FOAF.age,Literal(1997)))
 g.add((film,FOAF.name,Literal("toy story")))
 
 #info film
-g.add((film,attore,Literal("hoodie")))
-g.add((film,attore,Literal("buzz")))
-g.add((film,genere,Literal("Cartoon")))
+g.add((film,attore,Literal("http://www.imdb.com/name/hoodie")))
+g.add((film,attore,Literal("http://www.imdb.com/name/buzz")))
+g.add((film,genere,Literal("http://www.imdb.com/genr/Cartoon")))
 
-g.add((film,direttore,Literal("disney")))
-g.add((film,autore,Literal("Pixar")))
-
-
-g.add((film1,direttore,Literal("disney")))
-g.add((film1,genere,Literal("Cartoon")))
-g.add((film1,attore,Literal("hoodie")))
+g.add((film,direttore,Literal("http://www.imdb.com/company/disney")))
+g.add((film,autore,Literal("http://www.imdb.com/name/Pixar")))
 
 
-g.add((film2,direttore,Literal("RoccoAccademy")))
-g.add((film2,genere,Literal("Porno")))
-g.add((film2,attore,Literal("hoodie")))
-
-#g.add((id,FOAF.age,Literal(0.90)))
+g.add((film1,direttore,Literal("http://www.imdb.com/company/disney")))
+g.add((film1,genere,Literal("http://www.imdb.com/genr/Cartoon")))
+g.add((film1,attore,Literal("http://www.imdb.com/company/hoodie")))
 
 
-g.add((italia,pr,id))
-g.add((id,RDF.value,Literal(9.0)))
-'''
+g.add((film2,direttore,Literal("http://www.imdb.com/company/RoccoAccademy")))
+g.add((film2,genere,Literal("http://www.imdb.com/genr/Porno")))
+g.add((film2,attore,Literal("http://www.imdb.com/name/hoodie")))
+
+G = parseToGraph(g,["attore","direttore","autore"],["genere"])
+
+ 
+
 
 '''
 #G = parseToGraph(g,["direttore","attore","autore"],["genere"])
@@ -157,5 +184,8 @@ nx.draw_networkx_edge_labels(G, pos, labels=edge_labels,
 nx.draw(G, pos=pos, with_labels=True, node_size=200,font_size=13) 
 
 plt.show()
-
 '''
+
+
+#print(entity)
+
